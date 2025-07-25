8b73fd6938e3775474076077fa050588e48587ec
Ahmed Amr <ahmedamr5511aa@gmail.com>
2025-07-26 02:22:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,27 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      "https://www.misho.cfd",
-      "https://misho.cfd",
-      "http://localhost:3000",
-    ];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: ["https://www.misho.cfd", "https://misho.cfd", "http://localhost:3000"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "Accept",
-    "X-Requested-With",
-  ],
-  exposedHeaders: ["Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 
