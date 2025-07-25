1d12c3c892503093e11787640e0065f48455dad1
Ahmed Amr <ahmedamr5511aa@gmail.com>
2025-07-26 02:55:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,15 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "https://www.misho.cfd",
-  "https://misho.cfd",
-  "http://localhost:3000",
-];
-
+app.use(cors());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "https://www.misho.cfd",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type"],
   })
-);
+)
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
