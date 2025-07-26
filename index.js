a2613e6a529207d4110678befa3a326693429a0d
Ahmed Amr <ahmedamr5511aa@gmail.com>
2025-07-26 03:40:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ const customCors = require("./middleware/customCors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(customCors); // هنا الميدل وير للـ CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://www.misho.cfd");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
